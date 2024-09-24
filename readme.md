f29104ebe9b5ac00654a6ccaa6bcb8dd8f704bd2
jrm355 <jeremykazdan34@gmail.com>
2024-09-24 14:54:45 -0400

    commit 2

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # This is the most exclusive webpage in the world, only select few will enter
 ## tools used: using onling rescources as well as notes
+## How to get through 1: ok, 2: the-exclusive-passwword
 ### brief reflection: tbc
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("good luck")
+// console.log("good luck")
+// Step 1, simple alert to confirm you want to enter
+alert('are you sure you want to enter')
+
+// step 2, make sure you type the-exclusive-password to enter
+let correctPassword = "thepassword";
+let userPassword = "";
+
+while (userPassword !== correctPassword) {
+  userPassword = prompt("We need to see if you are clever enough to enter. Type thepassword to continue");
+
+  // If the user cancels the prompt, break the loop
+  if (userPassword === null) {
+    alert("Aren't even gonna try are you.");
+    break;
+  }
+
+  if (userPassword !== correctPassword) {
+    alert("this one is a layup. Try again.");
+  }
+}
+
+if (userPassword === correctPassword) {
+  alert("fine you pass");
+}
+
+//utilizing style
+const creepyText = document.getElementById("creepyText");
+creepyText.innerHTML = "Welcome to a place you shouldn't be";
+creepyText.style.fontFamily = "'Creepster', cursive";
+creepyText.style.color = "green";
+creepyText.style.fontSize = "36px"; 
